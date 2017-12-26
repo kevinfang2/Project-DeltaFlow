51bc9c3bf4889fe4268254ed9ea0a28a42ba0146
kevinfang2 <kevkev0123@gmail.com>
2017-12-26 03:17:55 -0800

    convert to webpack, pre sketchy stuff

--- test/Trades.js
+++ test/Trades.js
@@ -1,72 +1,57 @@
+contract('MetaCoin', function(accounts) {
+  it("should put 10000 MetaCoin in the first account", function() {
+    var meta = MetaCoin.deployed();
 
-describe("Trades Tests", function () {
-
-    var Trades = artifacts.require("./Trades.sol");
-
-    contract('Trades', function (accounts) {
-        it("should track sent trades for an entity and be retrievable by reference code", function () {
-
-            var accountA = "0x2945df8f0a8d3b8971db3a4408433ea049f0c843";
-            var tradeReferenceCode = web3.fromUtf8('trade-reference-code-1');
-
-            // Create an trade
-            return Trades.deployed().then(function (instance) {
-                return instance.addSentTrade(accountA, tradeReferenceCode)
-                    .then(function () {
-                        return instance.getSentTrade(accountA, 0)
-                    })
-                    .then(function (returnValue) {
-                        assert.equal(web3.toUtf8(returnValue), web3.toUtf8(tradeReferenceCode));
-                    });
-            });
-        });
+    return meta.getBalance.call(accounts[0]).then(function(balance) {
+      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
     });
+  });
+  it("should call a function that depends on a linked library  ", function(){
+    var meta = MetaCoin.deployed();
+    var metaCoinBalance;
+    var metaCoinEthBalance;
 
-    contract('Trades', function (accounts) {
-        it("should track received trades for an entity and be retrievable by reference code", function () {
+    return meta.getBalance.call(accounts[0]).then(function(outCoinBalance){
+      metaCoinBalance = outCoinBalance.toNumber();
+      return meta.getBalanceInEth.call(accounts[0]);
+    }).then(function(outCoinBalanceEth){
+      metaCoinEthBalance = outCoinBalanceEth.toNumber();
 
-            var accountA = "0x2945df8f0a8d3b8971db3a4408433ea049f0c843";
-            var tradeReferenceCode = web3.fromUtf8('trade-reference-code-2');
+    }).then(function(){
+      assert.equal(metaCoinEthBalance,2*metaCoinBalance,"Library function returned unexpeced function, linkage may be broken");
 
-            // Create an trade
-            return Trades.deployed().then(function (instance) {
-                return instance.addReceivedTrade(accountA, tradeReferenceCode)
-                    .then(function () {
-                        return instance.getReceivedTrade(accountA, 0)
-                    })
-                    .then(function (returnValue) {
-                        assert.equal(web3.toUtf8(returnValue), web3.toUtf8(tradeReferenceCode));
-                    });
-            });
-        });
     });
+  });
+  it("should send coin correctly", function() {
+    var meta = MetaCoin.deployed();
 
-    contract('Trades', function (accounts) {
-        it("should track sent and received trades for an entity and be retrievable by reference code", function () {
+    // Get initial balances of first and second account.
+    var account_one = accounts[0];
+    var account_two = accounts[1];
 
-            var accountA = "0x2945df8f0a8d3b8971db3a4408433ea049f0c843";
-            var tradeReferenceCode1 = web3.fromUtf8('trade-reference-code-1');
-            var tradeReferenceCode2 = web3.fromUtf8('trade-reference-code-2');
+    var account_one_starting_balance;
+    var account_two_starting_balance;
+    var account_one_ending_balance;
+    var account_two_ending_balance;
 
-            // Create an trade
-            return Trades.deployed().then(function (instance) {
-                return instance.addReceivedTrade(accountA, tradeReferenceCode1)
-                    .then(function () {
-                        return instance.getReceivedTrade(accountA, 0)
-                    })
-                    .then(function (returnValue) {
-                        assert.equal(web3.toUtf8(returnValue), web3.toUtf8(tradeReferenceCode1));
-                    })
-                    .then(function () {
-                        return instance.addReceivedTrade(accountA, tradeReferenceCode2)
-                    })
-                    .then(function () {
-                        return instance.getReceivedTrade(accountA, 1)
-                    })
-                    .then(function (returnValue) {
-                        assert.equal(web3.toUtf8(returnValue), web3.toUtf8(tradeReferenceCode2));
-                    });
-            });
-        });
+    var amount = 10;
+
+    return meta.getBalance.call(account_one).then(function(balance) {
+      account_one_starting_balance = balance.toNumber();
+      return meta.getBalance.call(account_two);
+    }).then(function(balance) {
+      account_two_starting_balance = balance.toNumber();
+      return meta.sendCoin(account_two, amount, {from: account_one});
+    }).then(function() {
+      return meta.getBalance.call(account_one);
+    }).then(function(balance) {
+      account_one_ending_balance = balance.toNumber();
+      return meta.getBalance.call(account_two);
+    }).then(function(balance) {
+      account_two_ending_balance = balance.toNumber();
+
+      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
     });
+  });
 });
